0ea63838715369a9a711bff780f0ad19d13507fb
MarkScrivo <84589133+MarkScrivo@users.noreply.github.com>
2024-12-13 00:17:46 -0500

    Simplify Chrome extension: remove background script, use default side panel

--- chrome-extension/background.js
+++
@@ -1,8 +0,0 @@
-// Enable side panel when extension is installed
-chrome.runtime.onInstalled.addListener(() => {
-  // Set default side panel options
-  chrome.sidePanel.setOptions({
-    enabled: true,
-    path: 'sidebar.html'
-  });
-});
