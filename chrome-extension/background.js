50b6a6e1081e3ca7a9e09fd281e51d378479c391
MarkScrivo <84589133+MarkScrivo@users.noreply.github.com>
2024-12-13 00:14:56 -0500

    Fix Chrome extension: update background script to use setOptions

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,5 +1,13 @@
 // Handle extension icon click
 chrome.action.onClicked.addListener((tab) => {
-  // Toggle the side panel
-  chrome.sidePanel.toggle();
+  // Show the side panel
+  chrome.sidePanel.setOptions({
+    enabled: true,
+    path: 'sidebar.html'
+  }).then(() => {
+    // The panel will be shown automatically when enabled
+    console.log('Side panel enabled');
+  }).catch((error) => {
+    console.error('Error showing side panel:', error);
+  });
 });
